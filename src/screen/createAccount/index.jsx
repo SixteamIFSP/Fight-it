b61df7c2892a97ffaa059b66a38233f1842811dc
kaamaral1096 <karini.sa@hotmail.com>
2022-08-20 15:37:07 -0300

    aplicando boas práticas

--- src/screen/createAccount/index.jsx
+++ src/screen/createAccount/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Text, View } from 'react-native'
+import { Text, View } from 'react-native';
 import { ButtonLinguage } from '../../components/buttonChangeLinguage';
 import { DoubleButtonConfirmation } from '../../components/doubleButtonConfirmation';
 import { Input } from '../../components/input';
@@ -9,12 +9,11 @@ import { styles as stylesGlobal } from '../../global/styles';
 import { styles } from './styles';
 import { SwitchButton } from '../../components/switchbutton';
 import { Loading } from '../../components/loading';
-import { toastMessage } from '../../util/toastMessage'; 
+import { toastMessage } from '../../util/toastMessage';
 
-export function CreateAccount({ navigation, routes }) {
-    const { t } = useTranslation()
-    const [loading, setLoading] =useState(false);
-
+export function CreateAccount({ navigation }) {
+    const { t: translation } = useTranslation()
+    const [loading, setLoading] = useState(false);
     const [name, setName] = useState('');
     const [mail, setMail] = useState('');
     const [phone, setPhone] = useState('');
@@ -43,11 +42,11 @@ export function CreateAccount({ navigation, routes }) {
             await createAccount(data, typeTeacher);
             setLoading(false);
             navigation.navigate('Login');
-            
         } else {
             toastMessage(false, "Digite os campos corretamente!")
         }
-    }
+    };
+
     function handleBack() {
         navigation.navigate('Login');
     };
@@ -55,37 +54,33 @@ export function CreateAccount({ navigation, routes }) {
     return (
         <View style={stylesGlobal.container}>
             <ButtonLinguage></ButtonLinguage>
-
             <Text style={styles.TitleLogin}>Fight It</Text>
-
             <View style={styles.userTypeChoice}>
-                <Text>{t('createAccount.descriptionSwitch')}</Text>
+                <Text>{translation('createAccount.descriptionSwitch')}</Text>
             </View>
-
             <View style={styles.switchButtons}>
                 <SwitchButton
                     onPress={() => setTypeTeacher(false)}
-                    text={t('createAccount.student')}
+                    text={translation('createAccount.student')}
                     type={!typeTeacher}
                 ></SwitchButton>
                 <SwitchButton
                     onPress={() => setTypeTeacher(true)}
-                    text={t('createAccount.teacher')}
+                    text={translation('createAccount.teacher')}
                     type={typeTeacher}
                 ></SwitchButton>
             </View>
-
             <Input
                 style={styles.inputes}
                 onChangeText={setName}
                 value={name}
-                placeholder={t('createAccount.name')}
+                placeholder={translation('createAccount.name')}
             />
             <Input
                 style={styles.inputes}
                 onChangeText={setMail}
                 value={mail}
-                placeholder={t('login.mail')}
+                placeholder={translation('login.mail')}
                 keyboardType="email-address"
             />
             <Input
@@ -93,35 +88,32 @@ export function CreateAccount({ navigation, routes }) {
                 keyboardType='phone-pad'
                 onChangeText={setPhone}
                 value={phone}
-                placeholder={t('createAccount.phone')}
+                placeholder={translation('createAccount.phone')}
             />
             <Input
                 style={styles.inputes}
                 onChangeText={setPass}
                 value={pass}
-                placeholder={t('login.password')}
+                placeholder={translation('login.password')}
                 secureTextEntry={true}
             />
             <Input
                 style={styles.inputes}
                 onChangeText={setConfirm}
                 value={confirm}
-                placeholder={t('createAccount.confirmPassword')}
+                placeholder={translation('createAccount.confirmPassword')}
                 secureTextEntry={true}
             />
             <View style={styles.confirmationButton}>
-
                 {
-                !loading ? 
-                    <DoubleButtonConfirmation
-                        handleConfirm={handleConfirm}
-                        handleBack={handleBack} />
-                :
-                    <Loading loading={loading} size={18}/>
+                    !loading ?
+                        <DoubleButtonConfirmation
+                            handleConfirm={handleConfirm}
+                            handleBack={handleBack} />
+                        :
+                        <Loading loading={loading} size={18} />
                 }
-                    
             </View>
-
         </View>
     )
 }
