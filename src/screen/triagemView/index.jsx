94cf2cebeffc7c7ed28470baba2fea1e8e3d9ce0
kaamaral1096 <karini.sa@hotmail.com>
2022-11-21 13:20:02 -0300

    add dados triagem

--- src/screen/triagemView/index.jsx
+++ src/screen/triagemView/index.jsx
@@ -4,7 +4,7 @@ import { ScrollView } from "react-native";
 import { getTriagem } from "../../controler/triagem";
 import { Container, TextAnswer, TextQuestion, TextTitle } from './style'
 
-export function TriagemView({ route, route:{params}}) {
+export function TriagemView({ route, route: { params } }) {
     const { t } = useTranslation();
     const { studantId } = params;
     const [dadosTriagem, setDadosTriagem] = useState(null)
@@ -19,33 +19,33 @@ export function TriagemView({ route, route:{params}}) {
 
     return (
         <ScrollView style={{ flex: 1, padding: 10 }}>{
-            !dadosTriagem ? 
-            <Container>
-                <TextTitle>{t('sortingView.title')}</TextTitle>
-                <TextQuestion>{t('sortingView.question')}</TextQuestion>
-            </Container>
-            :
-            <Container>
+            !dadosTriagem ?
+                <Container>
+                    <TextTitle>{t('sortingView.title')}</TextTitle>
+                    <TextQuestion>{t('sortingView.question')}</TextQuestion>
+                </Container>
+                :
+                <Container>
 
-                <TextQuestion>{t('sortingView.height')}</TextQuestion>
-                <TextAnswer>{dadosTriagem?.Altura}</TextAnswer>
+                    <TextQuestion>{t('sortingView.height')}</TextQuestion>
+                    <TextAnswer>{dadosTriagem?.Altura}</TextAnswer>
 
-                <TextQuestion>{t("sortingView.weight")}</TextQuestion>
-                <TextAnswer>{dadosTriagem?.Peso}</TextAnswer>
+                    <TextQuestion>{t("sortingView.weight")}</TextQuestion>
+                    <TextAnswer>{dadosTriagem?.Peso}</TextAnswer>
 
-                <TextQuestion>{t('sortingView.anamneseInjuries')} </TextQuestion>
-                <TextAnswer>{dadosTriagem?.Lesoes}</TextAnswer>
+                    <TextQuestion>{t('sortingView.anamneseInjuries')} </TextQuestion>
+                    <TextAnswer>{dadosTriagem?.Lesoes}</TextAnswer>
 
-                <TextQuestion>{t('sortingView.anamneseDidOrthProb')} </TextQuestion>
-                <TextAnswer>{dadosTriagem?.Problema_Ortopedico}</TextAnswer>
+                    <TextQuestion>{t('sortingView.anamneseDidOrthProb')} </TextQuestion>
+                    <TextAnswer>{dadosTriagem?.Problema_Ortopedico}</TextAnswer>
 
-                <TextQuestion>{t('sortingView.anamneseInjuries')} </TextQuestion>
-                <TextAnswer> {dadosTriagem?.Doencas_Cronicas}</TextAnswer>
+                    <TextQuestion>{t('sortingView.anamneseInjuries')} </TextQuestion>
+                    <TextAnswer> {dadosTriagem?.Doencas_Cronicas}</TextAnswer>
+                    
+                    <TextQuestion>{t('sortingView.anamneseComments1')}</TextQuestion>
+                    <TextAnswer>{dadosTriagem?.Comentario ? dadosTriagem : t('sortingView.noComments')}</TextAnswer>
 
-                <TextQuestion>{t('sortingView.anamneseComments1')}</TextQuestion>
-                <TextAnswer>{dadosTriagem?.Comentario || t('sortingView.noComments')}</TextAnswer>
-
-            </Container>
+                </Container>
         }
         </ScrollView>
     )
